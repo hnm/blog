a7dc98ed14510ee8b0ee24ec1bad0c91749586d7
tomekk-hnm <jansen@hnm.ch>
2020-09-01 12:01:35 +0200

    Update blogArticles.html.php

--- src/app/blog/view/blogArticles.html.php
+++ src/app/blog/view/blogArticles.html.php
@@ -1,9 +1,13 @@
 <?php
 	use n2n\impl\web\ui\view\html\HtmlView;
 	use page\ui\PageHtmlBuilder;
+	use blog\model\BlogDao;
 	
 	$view = HtmlView::view($view);
 	$html = HtmlView::html($view);
+	
+	$blogDao = $view->lookup(BlogDao::class);
+	$view->assert($blogDao instanceof BlogDao);
 
 	$pageHtml = new PageHtmlBuilder($view);
 	
@@ -12,7 +16,7 @@
 
 <?php $pageHtml->contentItems('top') ?>
 
-<?php if (!empty($blogArticles = $ddDao->getBlogArticles())): ?>
+<?php if (!empty($blogArticles = $blogDao->getBlogArticles())): ?>
     <div class="row justify-content-center blog-articles">
     	<?php foreach ($blogArticles as $blogArticle): ?>
     		<?php $view->import('inc\blogArticleNested.html', ['blogArticle' => $blogArticle]) ?>
@@ -21,4 +25,3 @@
 <?php endif ?>
 
 <?php $pageHtml->contentItems('bottom') ?>
-
